1f0804c6ff8e904e99bc88591af513d6f508b123
Matt Yoho <mby@mattyoho.com>
2010-05-27 23:30:04 -0500

    Add RDoc for restful_rails

--- lib/basic_assumption/default_assumption/restful_rails.rb
+++ lib/basic_assumption/default_assumption/restful_rails.rb
@@ -1,14 +1,15 @@
 module BasicAssumption
   module DefaultAssumption
+    # Restful default behavior in the context of Rails
     class RestfulRails < BasicAssumption::DefaultAssumption::Base
       attr_reader :action,
                   :lookup_id,
                   :name,
                   :page,
                   :per_page,
-                  :resource_attributes
+                  :resource_attributes #:nodoc:
 
-      def initialize(name, params)
+      def initialize(name, params) #:nodoc:
         @action    = params['action']
         @lookup_id = params['id']
         @name      = name.to_s
@@ -19,6 +20,35 @@ module BasicAssumption
         end
       end
 
+      # Returns a block that will attempt to do the correct thing depending
+      # on which action the request it triggering. If the action is 'show',
+      # 'edit', 'update', or 'destroy', then +assume+ will find an instance of
+      # an ActiveRecord model based on the +name+ that it recieved and an id
+      # value in the parameters. If the action is 'new' or 'create', +assume+
+      # will instantiate a new instance of the model class, passing in the
+      # values it finds in the +params+ hash with a key of the singularized
+      # form of the +name+ passed to +assume+. For example:
+      #
+      #    class WidgetController < ApplicationController
+      #      default_assumption :restful_rails
+      #      assume :widget
+      #
+      #      def create
+      #        widget.save!    # widget is: Widget.new(params[:widget])
+      #      end
+      #    end
+      #
+      # Note the object will have been instantiated but not saved, destroyed,
+      # etc. If the action is 'index', there are two possibilities for the
+      # behavior of +assume+. If the +name+ passed is of singular form, then
+      # a find will be performed, just as for a show or edit action. If the
+      # +name+ is a plural word, then +assume+ will find all instances of
+      # the model class.
+      #
+      # However, if the model responds to +paginate+ and there is a +page+
+      # key in the +params+ hash, +assume+ will attempt to paginate the
+      # results, also observing a +per_page+ value in the +params+ hash or
+      # defaulting to 15 if one is not found.
       def block
         klass = self.class
         Proc.new do |name|
@@ -26,7 +56,7 @@ module BasicAssumption
         end
       end
 
-      def result
+      def result #:nodoc:
         if list?
           list
         elsif make?
@@ -38,11 +68,11 @@ module BasicAssumption
 
       protected
 
-      def find?
+      def find? #:nodoc:
         %w(show edit update destroy).include? action
       end
 
-      def list
+      def list #:nodoc:
         if page?
           model_class.paginate('page' => page, 'per_page' => per_page)
         else
@@ -50,27 +80,27 @@ module BasicAssumption
         end
       end
 
-      def list?
+      def list? #:nodoc:
         action.eql?('index') && plural_name.eql?(name)
       end
 
-      def make?
+      def make? #:nodoc:
         %w(new create).include? action
       end
 
-      def model_class
+      def model_class #:nodoc:
         @model_class ||= name.classify.constantize
       end
 
-      def page?
+      def page? #:nodoc:
         page.present? && model_class.respond_to?(:paginate)
       end
 
-      def plural_name
+      def plural_name #:nodoc:
         name.pluralize
       end
 
-      def singular_name
+      def singular_name #:nodoc:
         name.singularize
       end
     end
